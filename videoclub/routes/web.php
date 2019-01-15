0d3c825274a7a775b4a740dfed26deec4ae1b570
alejandro2asir <43990189+alejandro2asir@users.noreply.github.com>
2019-01-15 09:51:02 +0100

    Create web.php

---
+++ videoclub/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+Route::get('/', function () {
+return view('home');
+});
+
+Route::get('/login' , function() {
+return view('auth.login');
+});
+
+Route::get('/catalog' , function() {
+return view('catalog.index');
+});
+
+Route::get('/catalog/show/{id}' , function($id) {
+return view('catalog.show', array('id'=>$id));
+});
+
+Route::get('/catalog/create' , function() {
+return view('catalog.create');
+});
+
+Route::get('/catalog/edit/{id}' , function($id) {
+return view('catalog.edit', array('id'=>$id));
+});
+
+Route::get('/logout' , function() {
+return 'Logout usuario';
+});
+
+#Route::get('/catalog/show/{id}' , function() {
+#return 'Vista detalle pelicula {id}';
+#});
+
+#Route::get('/catalog/create' , function() {
+#return 'Añadir pelicula';
+#});
+
+
+
+?>
