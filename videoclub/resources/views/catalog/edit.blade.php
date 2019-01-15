15d2e5b26a7d596cce20c928b0ca511df77496b9
alejandro2asir <43990189+alejandro2asir@users.noreply.github.com>
2019-01-15 09:50:40 +0100

    Create edit.blade.php

---
+++ videoclub/resources/views/catalog/edit.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+  	@extends('layouts.master')
+	@section('content')
+		Modificar peliculas
+	@stop
+  </body>
+</html>
