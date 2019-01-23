f1f5ddbe6528efa4c91e25667e8713000002e5db
alejandro2asir <43990189+alejandro2asir@users.noreply.github.com>
2019-01-23 19:52:37 +0100

    Update web.php

--- videoclub/routes/web.php
+++ videoclub/routes/web.php
@@ -1,43 +1,6 @@
 <?php
 
-#Route::get('/', function () {
-#return view('home');
-#});
-
-Route::get('/login' , function() {
-return view('auth.login');
-});
-
-Route::get('/catalog' , function() {
-return view('catalog.index');
-});
-
-Route::get('/catalog/show/{id}' , function($id) {
-return view('catalog.show', array('id'=>$id));
-});
-
-Route::get('/catalog/create' , function() {
-return view('catalog.create');
-});
-
-Route::get('/catalog/edit/{id}' , function($id) {
-return view('catalog.edit', array('id'=>$id));
-});
-
-Route::get('/logout' , function() {
-return 'Logout usuario';
-});
-
-#Route::get('/catalog/show/{id}' , function() {
-#return 'Vista detalle pelicula {id}';
-#});
-
-#Route::get('/catalog/create' , function() {
-#return 'Añadir pelicula';
-#});
-
-#CONTROLLERS
-
+Route::group(['middleware' => 'auth'], function (){
 Route::get('/', 'HomeController@getHome');
 
 Route::get('/catalog', 'CatalogController@getIndex');
@@ -48,5 +11,49 @@ Route::get('/catalog/create', 'CatalogController@getCreate');
 
 Route::get('/catalog/edit/{id}', 'CatalogController@getEdit');
 
+Route::post('/catalog/create', 'CatalogController@postCreate');
 
-?>
+Route::put('/catalog/edit/{id}', 'CatalogController@putEdit');
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+#Route::get('/', function () {
+#return view('home');
+#});
+
+#Route::get('/login' , function() {
+#return view('auth.login');
+#});
+
+#Route::get('/logout' , function() {
+#return 'Logout usuario';
+#});
+
+#Route::get('/catalog/show/{id}' , function() {
+#return 'Vista detalle pelicula {id}';
+#});
+
+#Route::get('/catalog/create' , function() {
+#return 'Añadir pelicula';
+#});
+
+#Route::get('/catalog' , function() {
+#return view('catalog.index');
+#});
+
+#Route::get('/catalog/show/{id}' , function() {
+#return view('catalog.show');
+#});
+
+#Route::get('/catalog/create' , function() {
+#return view('catalog.create');
+#});
+
+#Route::get('/catalog/edit/{id}' , function() {
+#return view('catalog.edit');
+#});
